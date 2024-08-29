e505f7a0108f33d714d3ef70d1cb02a66604bf0f
m1a2023 <mag2017go@gmail.com>
2024-08-29 22:06:36 +0500

    init commit

---
+++ src/main/java/org/axpen/restapi/AxpenApplication.java
@@ -0,0 +1,13 @@
+package org.axpen.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxpenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxpenApplication.class, args);
+	}
+
+}
